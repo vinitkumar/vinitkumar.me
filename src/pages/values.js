6944bbcf0b76768e213fa80380fa37f8508ab73d
Vinit Kumar <vinit@getchip.uk>
2019-07-22 20:27:39 +0530

    add support for images

--- src/pages/values.js
+++ src/pages/values.js
@@ -1,11 +1,15 @@
 import React from "react"
+import Unsplash from "react-unsplash-wrapper"
 
 import Layout from "../components/layout"
-
+// wT5VXP92jFA
 class ValueComponent extends React.Component {
   render() {
     return (
         <Layout location={this.props.location} title="Values">
+          <div style={{position: 'relative', width: '100%', height: 400, margin: 'auto', marginBottom: 60}}>
+            <Unsplash expand keywords="worship" />
+          </div>
             <ul>
               <li>
                   Fast learner who can grasp new tech quickly and apply them to on-going projects.
