34ceb1c4de729bc4d25ae2313f44d6eb5c5ad986
Vinit Kumar <mail@vinitkumar.me>
2019-08-21 15:02:24 +0530

    feat: change image address to something that I like

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -14,7 +14,7 @@ class BlogPostTemplate extends React.Component {
     const post = this.props.data.markdownRemark
     const siteTitle = this.props.data.site.siteMetadata.title
     const { previous, next } = this.props.pageContext
-    let unsplashURL = `https://images.unsplash.com/photo-1508643315917-6688bbbb4cb5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80`;
+    let unsplashURL = `https://images.unsplash.com/photo-1507842217343-583bb7270b66?ixlib=rb-1.2.1&auto=format&fit=crop&w=1453&q=80`;
     return (
       <Layout location={this.props.location} title={siteTitle}>
         <Helmet>
