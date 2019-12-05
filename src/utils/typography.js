2d18f70fdce2aed40960ef62d64574bbf2c85ed5
Vinit Kumar <mail@vinitkumar.me>
2019-12-05 18:15:15 +0530

    feat: change fonts

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,18 +1,8 @@
 import Typography from "typography"
-import Wordpress2016 from "typography-theme-wordpress-2016"
+import funstonTheme from 'typography-theme-funston'
 
-Wordpress2016.overrideThemeStyles = () => {
-  return {
-    "a.gatsby-resp-image-link": {
-      boxShadow: `none`,
-    },
-  }
-}
-
-delete Wordpress2016.googleFonts
-
-const typography = new Typography(Wordpress2016)
-
+funstonTheme.baseFontSize = '16px'
+const typography = new Typography(funstonTheme)
 // Hot reload typography in development.
 if (process.env.NODE_ENV !== `production`) {
   typography.injectStyles()
