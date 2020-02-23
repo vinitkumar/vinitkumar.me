abb991cf6b285d02efd563593383af87c07eb6bf
Vinit Kumar <mail@vinitkumar.me>
2020-02-23 21:01:22 +0530

    feat: fix marginleft to the bottom footer

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -59,6 +59,7 @@ class BlogPostTemplate extends React.Component {
             flexWrap: `wrap`,
             justifyContent: `space-between`,
             listStyle: `none`,
+            marginLeft: 0,
             padding: 0,
           }}
         >
