5e7d027f24873365b9d4de3fd960a205b7986a8a
Vinit Kumar <mail@vinitkumar.me>
2020-09-22 13:34:15 +0530

    feat: add rituparnadey.com domain

--- src/pages/about.js
+++ src/pages/about.js
@@ -53,7 +53,7 @@ const AboutIndex  = (props) => {
 
     <p>Currently, I lead the development  and architecture of multi-tenant & distributed CMS system at Socialschools B.V <span role="img" aria-label="dutch">🇳🇱</span> . I have 8 years of experience writing backend in Django for a successful product and numerous client projects. You can download my <a target='_blank'  rel="noopener noreferrer"  href="https://vinitkumar.github.io/vinit_kumar.pdf"> resume</a> here. My biggest strength is in coming up with simple solutions for difficult problems.
     </p>
-    <p>I specially enjoy reading books, and <strong><i>spending time</i></strong> with my family. I live in Pune with my family. My wife Rituparna is the founder of a Digital Marketing Company <a href="https://scoophubs.com" target="_blank" rel="noopener noreferrer">ScoopHubs.com</a></p>
+    <p>I specially enjoy reading books, and <strong><i>spending time</i></strong> with my family. I live in Pune with my family. My wife <a target='_blank'  rel="noopener noreferrer"  href="https://rituparnadey.com">Rituparna Dey</a> is the founder of a Digital Marketing Company <a href="https://scoophubs.com" target="_blank" rel="noopener noreferrer">ScoopHubs.com</a></p>
     <p>I contribute to some open source software and you can browse them at my github profile <a href="https://github.com/vinitkumar" target="_blank" rel="noopener noreferrer">vinitkumar</a>.
       If you use twitter, you can follow me <a target="_blank" rel="noopener noreferrer" href="https://twitter.com/vinitkme">@vinitkme</a> on twitter.
     I am on LinkedIn, connect with <a target="_blank"  rel="noopener noreferrer" href="https://www.linkedin.com/in/vinitatlinkedin/">vinitatlinkedin</a> at LinkedIn.
