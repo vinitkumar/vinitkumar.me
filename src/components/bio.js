f9bdc654a2347ef0712dbf78413c2e3dbb724c31
Vinit Kumar <mail@vinitkumar.me>
2020-10-29 00:02:19 +0530

    feat: improve bio ui and wording

--- src/components/bio.js
+++ src/components/bio.js
@@ -44,20 +44,12 @@ const Bio = () => {
       }}
     >
       <p style={{
-        width: `80%`,
-        border: `1px solid #eeeeee`,
-        padding: `12px`,
+        width: `100%`,
       }}>
-        Hi there, I am <strong><a href={twitterUrl}>{author}</a></strong>. I am a software
-        engineer from India <span role="img" aria-label="india flag">🇮🇳</span> who loves making computers do things and admires good books, music, art
+        Hello Hello! I am <strong><a href={twitterUrl}>{author}</a></strong>. I am a software
+        engineer who loves making computers do things and admires good books, music, art
     and <span style={{textDecorationLine: `line-through` }}>coffee</span>.
         You can read more <Link to="/about">about</Link> me.
-      </p>
-
-      <p style={{
-        border: `1px solid #eeeeee`,
-        padding: `12px`,
-      }}>
         Find my
         {` `}
         <a href="https://vinitkumar.github.io/vinit_kumar.pdf"
@@ -84,7 +76,7 @@ const Bio = () => {
             textDecoration: `none`,
           }}
         >
-          &nbsp;code
+        code
         </a> here.
       </p>
     </div>
