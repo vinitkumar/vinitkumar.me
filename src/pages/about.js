d8b8bb745e870e15f0a97bbd879d80cd1fb72cc1
Vinit Kumar <mail@vinitkumar.me>
2021-09-05 00:47:12 +0530

    feat: a better about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -44,16 +44,17 @@ const AboutIndex  = (props) => {
       }}
     />
     <p>
-      My name is <strong>Vinit Kumar</strong> and I'm a Software Engineer..
-      I am mainly interested in designing and building highly scalable systems in Python, Node.js, and Go.I like Vim and good coffee. In this website, you will find my essays on a variety of topics.
+      My name is <strong>Vinit Kumar</strong>, and I'm a Software Engineer.<br/>
+      <br/>
+      I am mainly interested in designing and building highly scalable systems in Python, Node.js, and Go. I like writing code in Vim/NeoVim and a good coffee to start my day. In this website, you will find my writing on a variety of topics.
     </p>
 
-    <p>Currently, I lead the development and architecture of multi-tenant & distributed CMS system at Socialschools B.V. I have more than 8 years of experience writing backend in Django for a successful product and numerous client projects. You can download my <a target='_blank'  rel="noopener noreferrer"  href="https://vinitkumar.github.io/vinit_kumar.pdf"> resume</a> here. My biggest strength is in coming up with simple solutions for difficult problems.
+    <p>Currently, I lead the development and architecture of a multi-tenant & distributed CMS at Socialschools B.V. I have more than 8 years of experience writing backend in Django for a couple of successful product and numerous client projects. You can download my <a target='_blank'  rel="noopener noreferrer"  href="https://vinitkumar.github.io/vinit_kumar.pdf"> resume</a> here. My biggest strength is in coming up with simple solutions for difficult problems.
     </p>
     <p>I specially enjoy reading books, and <strong><i>spending time</i></strong> with my family. I live in Pune with my family. My wife <a target='_blank'  rel="noopener noreferrer"  href="https://rituparnadey.com">Rituparna Dey</a> is the founder of a Digital Marketing Company <a href="https://scoophubs.com" target="_blank" rel="noopener noreferrer">ScoopHubs.com</a></p>
     <p>I contribute to some open source software and you can browse them at my github profile <a href="https://github.com/vinitkumar" target="_blank" rel="noopener noreferrer">vinitkumar</a>.
       If you use twitter, you can follow me <a target="_blank" rel="noopener noreferrer" href="https://twitter.com/vinitkme">vinitkme</a> on twitter.
-    I am also on LinkedIn, connect with me on linkedin at <a target="_blank"  rel="noopener noreferrer" href="https://www.linkedin.com/in/vinitatlinkedin/">vinitatlinkedin</a>.
+    I am also on <a target="_blank"  rel="noopener noreferrer" href="https://www.linkedin.com/in/vinitatlinkedin/">LinkedIn</a>.
     </p>
     <p>If you use any of my OSS software, please consider sponsoring my work <span rel="img" aria-label="heart-emoji">❤️ </span>, so that I can justify spending time on OSS that benefits you all. You can sponsor me on my <a href="https://opencollective.com/vinit-kumar" rel="noopener noreferrer" target="_blank">opencollective </a> here.</p>
     <iframe src="https://github.com/sponsors/vinitkumar/card" title="Sponsor vinitkumar" height="225" width="600" style={{ "border":
