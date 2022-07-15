268d0b536b50b74d1b318caf96734ff156c8acef
Vinit Kumar <vinit.kumar@kidskonnect.nl>
2022-07-16 00:53:44 +0530

    feat: add new tag and see if it works

--- gatsby-config.js
+++ gatsby-config.js
@@ -55,7 +55,7 @@ module.exports = {
       options: {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
         trackingIds: [
-          `UA-157969207-1`
+          `G-FGECNRP32R`
         ],
         // This object gets passed directly to the gtag config command
         // This config will be shared across all trackingIds
