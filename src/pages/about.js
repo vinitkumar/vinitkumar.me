0ed32ed22014841f739033358c5b8d24f3608a4f
Vinit Kumar <vinit.kumar@kidskonnect.nl>
2022-07-16 00:49:10 +0530

    feat: update about

--- src/pages/about.js
+++ src/pages/about.js
@@ -44,12 +44,12 @@ const AboutIndex  = (props) => {
       }}
     />
     <p>
-      My name is <strong>Vinit Kumar</strong>, and I'm a Software Engineer.<br/>
+      My name is <strong>Vinit Kumar</strong>, and I'm a Staff Engineer at Socialschools Websites Team.<br/>
       <br/>
-      Designing and building highly scalable systems in Python, Node.js, and Go. I like writing code in Vim and a good coffee to start my day. 
+      I have written a multi-tenant backend system that scales to more than 50m requests/month and powers a profitable business. 
     </p>
 
-    <p>I lead the development and architecture of a multi-tenant & distributed CMS at Socialschools B.V. I have 9 years of experience writing backend in Django for a couple of successful products and numerous client projects. You can download my <a target='_blank'  rel="noopener noreferrer"  href="https://vinitkumar.github.io/vinit_kumar.pdf"> resume</a> here. My strength is in coming up with simple solutions for difficult problems.
+    <p>I lead the development and architecture of a multi-tenant CMS at Socialschools B.V. I have more than 9 years of experience writing backend in Django for a couple of successful products and numerous client projects. You can download my <a target='_blank'  rel="noopener noreferrer"  href="https://vinitkumar.github.io/vinit_kumar.pdf"> resume</a> here. My strength is in coming up with simple solutions for difficult problems.
     </p>
     <p>I enjoy reading books, and <strong><i>spending time</i></strong> with family. I live in Pune with my family. My wife <a target='_blank'  rel="noopener noreferrer"  href="https://rituparnadey.com">Rituparna Dey</a> is founder of a Digital Marketing Company <a href="https://scoophubs.com" target="_blank" rel="noopener noreferrer">ScoopHubs.com</a></p>
     <p>I contribute to some open source software and you can browse them at my github profile <a href="https://github.com/vinitkumar" target="_blank" rel="noopener noreferrer">vinitkumar</a>.
