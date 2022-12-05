73aa3ef52bed6936dd9ed7be21e7a7f92eab3fca
Vinit Kumar <vinit.kumar@kidskonnect.nl>
2022-12-06 02:17:25 +0530

    fix: link to values page

--- src/components/bio.js
+++ src/components/bio.js
@@ -45,7 +45,7 @@ const Bio = () => {
       <p style={{
         width: `100%`,
       }}>
-          As a <Link to="/about">Software Engineer</Link> with a passion for creating and problem-solving, I am constantly seeking new challenges and opportunities to push the boundaries of what computers can do. I am an avid admirer of good literature, music, art, and coffee. Explore my website to learn more about my skills, experience, and <Link to="/about">values</Link> as a programmer. Here, you can also find links to my <a href="https://vinitkumar.github.io/vinit-kumar.pdf" style={{textDecoration: `none`,}}>resume</a> and <a href="https://github.com/vinitkumar" style={{textDecoration: `none`,}}>code repositories</a> .
+          As a <Link to="/about">Software Engineer</Link> with a passion for creating and problem-solving, I am constantly seeking new challenges and opportunities to push the boundaries of what computers can do. I am an avid admirer of good literature, music, art, and coffee. Explore my website to learn more about my skills, experience, and <Link to="/values">values</Link> as a programmer. Here, you can also find links to my <a href="https://vinitkumar.github.io/vinit-kumar.pdf" style={{textDecoration: `none`,}}>resume</a> and <a href="https://github.com/vinitkumar" style={{textDecoration: `none`,}}>code repositories</a> .
       </p>
     </div>
   )
