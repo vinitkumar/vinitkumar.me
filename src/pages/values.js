26725dbd76b8b3ee3daf8d7531a57bd6f74a194b
Vinit Kumar <vinit.kumar@promobitech.com>
2026-01-13 16:10:19 +0530

    refactor: convert class components to functional with hooks
    - Convert 404.js, uses.js, values.js to functional components
    - Convert blog-post.js and til-post.js templates to functional
    - Convert index.js to functional using useState and useEffect hooks
    - Convert til.js index page to functional component
    - Use CSS variables instead of hardcoded colors
    - Cleaner, more maintainable code structure

--- src/pages/values.js
+++ src/pages/values.js
@@ -1,37 +1,34 @@
 import React from "react"
-
 import Layout from "../components/layout"
-// wT5VXP92jFA
-class ValueComponent extends React.Component {
-  render() {
-    return (
-        <Layout location={this.props.location} title="Values">
-          <ul>
-            <li>
-              Quickly adapts to new technologies and applies them to ongoing projects.
-            </li>
-            <li>
-              Dedicated to personal and professional growth and improvement.
-            </li>
-            <li>
-              Experienced in working independently or as part of a remote team.
-            </li>
-            <li>
-              Strong problem-solving and analytical abilities.
-            </li>
-            <li>
-              Resourceful and efficient, with a knack for improvisation.
-            </li>
-            <li>
-              Excellent written and verbal English communication skills.
-            </li>
-            <li>
-              Empathetic and ethical in all interactions.
-            </li>
-          </ul>
-        </Layout>
-    )
-  }
+
+const ValuesPage = ({ location }) => {
+  return (
+    <Layout location={location} title="Values">
+      <ul>
+        <li>
+          Quickly adapts to new technologies and applies them to ongoing projects.
+        </li>
+        <li>
+          Dedicated to personal and professional growth and improvement.
+        </li>
+        <li>
+          Experienced in working independently or as part of a remote team.
+        </li>
+        <li>
+          Strong problem-solving and analytical abilities.
+        </li>
+        <li>
+          Resourceful and efficient, with a knack for improvisation.
+        </li>
+        <li>
+          Excellent written and verbal English communication skills.
+        </li>
+        <li>
+          Empathetic and ethical in all interactions.
+        </li>
+      </ul>
+    </Layout>
+  )
 }
 
-export default ValueComponent
+export default ValuesPage
