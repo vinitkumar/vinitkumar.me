1fb64bae33fffde8703339fceea2c2a42ef0b117
Vinit Kumar <vinit.kumar@promobitech.com>
2026-07-27 15:58:49 +0530

    fix: prevent font shift on page refresh

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,4 +1,7 @@
 import * as React from "react"
+import berkaTextRegular from "./src/fonts/berka-text/BerkaText-Regular.woff2"
+import berkaTextSemiBold from "./src/fonts/berka-text/BerkaText-SemiBold.woff2"
+import berkaTextBold from "./src/fonts/berka-text/BerkaText-Bold.woff2"
 
 const themeScript = `
 (function() {
@@ -13,6 +16,30 @@ const themeScript = `
 
 export const onRenderBody = ({ setHeadComponents, setPreBodyComponents }) => {
   setHeadComponents([
+    <link
+      key="preload-berka-text-regular"
+      rel="preload"
+      href={berkaTextRegular}
+      as="font"
+      type="font/woff2"
+      crossOrigin="anonymous"
+    />,
+    <link
+      key="preload-berka-text-semibold"
+      rel="preload"
+      href={berkaTextSemiBold}
+      as="font"
+      type="font/woff2"
+      crossOrigin="anonymous"
+    />,
+    <link
+      key="preload-berka-text-bold"
+      rel="preload"
+      href={berkaTextBold}
+      as="font"
+      type="font/woff2"
+      crossOrigin="anonymous"
+    />,
     <meta
       key="theme-color-light"
       name="theme-color"
@@ -26,7 +53,7 @@ export const onRenderBody = ({ setHeadComponents, setPreBodyComponents }) => {
       media="(prefers-color-scheme: dark)"
     />,
   ])
-  
+
   setPreBodyComponents([
     <script
       key="theme-script"
